58b17feb6b50ec31468ac8737ffda10e26c289b8
Shifter447 <tbesdemiotis@gmail.com>
2024-12-03 09:53:23 +0100

    klaar

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+
+        }
+
+            internal void Run()
+            {
+            Console.WriteLine("Dit is nu de start van mijn programma"); 
+            
+            }
+      
+    }
+}
