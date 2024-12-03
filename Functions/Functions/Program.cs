ba208d595f925543ee79ebf918ee61970e971be8
Shifter447 <tbesdemiotis@gmail.com>
2024-12-03 10:33:35 +0100

    Klaar met functions 0.3

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -11,11 +11,52 @@ namespace Functions
 
         }
 
-            internal void Run()
-            {
+        internal void Run()
+        {
             Console.WriteLine("Dit is nu de start van mijn programma");
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+        }
             
-            }
-      
+        internal void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        
+        }
+        internal void Vraag2()
+        {
+            Console.WriteLine("Do you like pizza?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag3()
+        {
+            Console.WriteLine("What is your favorite game?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag4()
+        {
+            Console.WriteLine("How old are you?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag5()
+        {
+            Console.WriteLine("What is your most cherished memory?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
     }
+
 }
