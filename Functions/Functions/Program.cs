2ff7e659392714e7997caf5c4ae8ecaf2bb07df7
Shifter447 <tbesdemiotis@gmail.com>
2025-01-17 09:38:37 +0100

    01 klaar

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -4,6 +4,11 @@ namespace Functions
 {
     internal class Program
     {
+        string[] vragen = new string[] {
+            "What 1997 N64 video game, features James Bond and is named after the 1995 film?",
+            "What arcade game was called Puckman in Japan?"
+        };
+
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -11,6 +16,10 @@ namespace Functions
 
         }
 
+        internal string GetVraag(int vraagIndex)
+        {
+            return vragen[vraagIndex];
+        }
         internal void Run()
         {
             Console.WriteLine("Dit is nu de start van mijn programma");
@@ -19,6 +28,10 @@ namespace Functions
             Vraag3();
             Vraag4();
             Vraag5();
+            string antwoord6 = Vraag6();
+            Console.WriteLine(antwoord6);
+            string vraag0 = GetVraag(0);
+            Console.WriteLine(vraag0);
         }
             
         internal void Vraag1()
@@ -56,6 +69,20 @@ namespace Functions
             string antwoord = Console.ReadLine();
 
             Console.WriteLine(antwoord);
+        }
+        internal string Vraag6()
+        {
+            Console.WriteLine("Wat wil je later doen?");
+            string antwoord = Console.ReadLine();
+
+            return antwoord;  
+        }
+        internal string GetRandomVraag()
+        {
+            int random = ;
+            return random;
+
+;
         }
     }
 
---
+++ Rijschool/Rijschool/LesPakket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rijschool
+{
+    internal class LesPakket
+    {
+        internal int urenGekocht;
+        internal int urenGebruikt;
+        internal int examenPogingen;
+        internal bool automaat;
+    }
+}
