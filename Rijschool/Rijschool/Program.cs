5f5d056e38ee0235e03e6226406879ef4c407789
Shifter447 <tbesdemiotis@gmail.com>
2025-01-31 10:20:24 +0100

    0.2 halverwege

--- Rijschool/Rijschool/Program.cs
+++ Rijschool/Rijschool/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Run();
         }
+        internal void Run()
+        {
+            Auto auto1 = new Auto()
+            {
+                automaat = true,
+                kenteken = "AA824H",
+                KilometerStand = 20567,
+                merk = "Volkswagen"
+            };
+
+            Auto auto2 = new Auto()
+            {
+                automaat = true,
+                kenteken = "KU786G",
+                KilometerStand = 65739,
+                merk = "Volvo"
+            };
+
+            RijLeraar rijLeraar = new RijLeraar()
+            {
+                leeftijd = 30,
+                naam = "John",
+                zzp = false
+            };
+            LesUur lesUur = new LesUur()
+            {
+                auto = auto1,
+                rijLeraar = rijLeraar,
+
+                tijd = 1130
+            };
+
+            Student student = new Student()
+            {
+                naam = "Tom",
+                leeftijd = 21
+
+            };
+        }
+
     }
 }
